9def6443978727128bf0d1e89097687f304fa125
alexauden <40079710+alexauden@users.noreply.github.com>
2018-08-19 23:25:24 -0500

    Create programinfo.php

---
+++ programinfo.php
@@ -0,0 +1,107 @@
+<?php include("loginfunctions.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Music lessons in the Fox Valley area!</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/creative.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="index.php">Appleton Conservatory</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                    <a class="nav-link" href="#about">About</a>
+                    <div class="nav-item-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                    </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#services">Fall 2018 Program Guide</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">Upcoming Events</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    
+      <section>
+
+          <div id="info" class="info">
+            <div class="infoheader">
+                <p><a href="index.php">Home  </a>/  <a href="#">Classes  </a>/  <a href="programinfo.php">Conservatory Program</a></p>
+              <h1>Conservatory Program</h1>
+          </div>
+          <div id="conservatory">
+              <p>The Conservatory Program is for students ages 8+, and is for students who have some knowledge of their instrument already and are interested in studying a conservatory curriculum. Students will attend weekly lessons, and will attend weekly masterclass sessions with their teacher and other students. These students will be required to participate in our three annual recitals and will have the option to perform for many other events during the year.</p>
+              <br>
+    <h3>How do I sign up?</h3>
+              <p>Interested students should sign up online using our online registration system. Students who are new to their instrument are reccomended to enroll in 1 trimester of a group class for their instrument. This sets the foundation for a solid technique in a fun environment where students can learn from each other and get to know others who play their instrument.<br><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#login">Enroll now</button>  </p>
+              <p></p>
+          </div>
+<br>
+    <div id="group">
+          <h1>Group Classes</h1>
+        <p>Blah blah balh</p>
+          </div>
+          
+    <div id="adult">
+          <h1>Adult Classes</h1>
+              <p>Come Join the fun!</p>
+          </div>
+          
+          </div>
+      </section>
+      
+      
+   <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.min.js"></script>
+
+  </body>
+
+</html>
